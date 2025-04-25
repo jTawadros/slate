69169735379345383b6ff6134dab8daf85acd9d4
root <root@Jon.>
2025-04-25 14:45:41 -0700

    user email in account

--- frontend/src/components/Account.jsx
+++ frontend/src/components/Account.jsx
@@ -1,6 +1,12 @@
 import React from "react"
+import { auth } from "../firebase"
 
 
 export default function Account() {
-  return <div>Account Page</div>;
+
+  return(
+    <div>Account Page</div>;
+    
+    <h1>{auth.currentUser.email}</h1>
+  )
 }
