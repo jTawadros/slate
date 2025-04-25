12888bd3fc23477c3c2f55d5676951c353534a68
root <root@Jon.>
2025-04-25 15:12:18 -0700

    Add button syling

--- frontend/src/components/Account.jsx
+++ frontend/src/components/Account.jsx
@@ -14,9 +14,9 @@ export default function Account() {
       </div>
         <div>
           <h2>Account Settings</h2>
-          <button>Change Display Name</button>
-          <button>Upload Profile Picture</button>
-          <button>Update Password</button>
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Change Display Name</button>
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Upload Profile Picture</button>
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Update Password</button>
         </div>
 
     </form>
