448e2511c6e1b5457d6bfc7ae2e022a6c83e65f8
root <root@Jon.>
2025-04-25 18:06:43 -0700

    test

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCwIPWFzPXhKenHMhIS-eMiN4cPbECqcVE",
   authDomain: "slateworks-9b562.firebaseapp.com",
   projectId: "slateworks-9b562",
-  storageBucket: "slateworks-9b562.appspot.com",
+  storageBucket: "slateworks-9b562.firebasestorage.app",
   messagingSenderId: "879528630918",
   appId: "1:879528630918:web:d6397655d42910079ecf07",
   measurementId: "G-RC1VCBL8XN"
